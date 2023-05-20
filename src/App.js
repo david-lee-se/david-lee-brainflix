6b571d86d42c1af3f9ccb02c551be09caf1bf436
David <Dlee.business2015@gmail.com>
2023-05-20 09:30:35 -0700

    add comments section

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import ActiveVideoDetails from './components/ActiveVideoDetails/ActiveVideoDetai
 import { useState } from 'react';
 import videoDetails from './assets/data/video-details.json';
 import videos from './assets/data/videos.json';
+import Comments from './components/Comments/Comments';
 
 
 function App() {
-console.log(videoDetails)
+
 const [activeVideo, setActiveVideo] = useState(videoDetails[0]);
   return (
     <div className="App">
       <Header/>
       <VideoPlayer activeVideo={videos}/>
       <ActiveVideoDetails videoDetails={videoDetails}/>
+      <Comments 
+        videoDetails={videoDetails}
+        activeVideo={activeVideo}
+        setActiveVideo={setActiveVideo}
+      />
     </div>
   );
 }
---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,37 @@
+import './Comments.scss';
+
+
+function Comments(props) {
+
+
+
+    return (
+        <div className="comments">
+            <hr/>
+            <ul className="comments-list">
+                
+                {props.activeVideo.comments.map((comment) => {
+                    
+                    const date = new Date(comment.timestamp).toDateString();
+                    return (
+                        <div key={comment.id}>
+                            <li className='comment' >
+                                <div className='comment__avatar' alt='avatar'></div>
+                                <div className='comment__text-container'>
+                                <h4 className="comment__name">{comment.name}</h4>
+                                <p className="comment__date">{date}</p>
+                                <p className="comment__content">{comment.comment}</p>
+                                </div>
+                            </li>
+                            <hr/>
+                        </div>
+                            )
+                    })
+                  }
+                
+            </ul>
+        </div>
+    )
+}
+
+export default Comments;
