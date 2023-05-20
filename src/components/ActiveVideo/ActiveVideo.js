e67f052fd0251b2e9aba5449b56a87c9bb70ca37
David <Dlee.business2015@gmail.com>
2023-05-19 20:01:40 -0700

    add video player, forgot merge

---
+++ src/components/ActiveVideo/ActiveVideo.js
@@ -0,0 +1,14 @@
+
+
+function ActiveVideo(props) {
+
+    return (
+        <article>
+            <div className="active-video-container">
+                <video poster="#" className="active-video" controls ></video>
+            </div>
+        </article>
+    )
+}
+
+export default ActiveVideo;
