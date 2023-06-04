93dfe690f81f6f13b28cc3c20fa0416ab53bdaf4
David <Dlee.business2015@gmail.com>
2023-06-04 15:36:05 -0700

    add post functionality

--- client/src/pages/VideoUpload/VideoUpload.js
+++ client/src/pages/VideoUpload/VideoUpload.js
@@ -1,24 +1,63 @@
-import thumbnail from '../../assets/images/Upload-video-preview.jpg';
 import './VideoUpload.scss';
-import PublishForm from '../../components/PublishForm/PublishForm';
-import PublishButton from '../../components/PublishButton/PublishButton';
+import publishIcon from '../../assets/images/publish.svg';
+import {useId} from 'react';
+import axios from 'axios';
 
 function VideoUpload() {
+
+    const videoId= useId();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formEl = event.target;
+        const title = formEl.title.value;
+        const description = formEl.description.value;
+        const image = "http://localhost:8080/public/images/upload.jpg";
+        const id = formEl.description.id;
+        const videoObj = {title, description, image, id};
+        axios.post("http://localhost:8080/videoUpload", videoObj)
+            .then((result) => {
+                event.target.reset();
+            })
+    }
     return (
     <>
-        <section className='title-container'>
+        <form onSubmit={handleSubmit} className='title-container'>
             <h1 className='video-upload-title'>Upload Video</h1>
             <div className='upload-details'>
                 <div className='thumbnail-container'>
                     <h3 className='thumbnail__label'>VIDEO THUMBNAIL</h3>
-                    <img className="thumbnail__img" src={thumbnail} alt='thumbnail'/>
+                    <img className="thumbnail__img" src="http://localhost:8080/public/images/upload.jpg" alt='thumbnail'/>
                 </div>
-                <PublishForm/>
+                <div className='publish-form__input-container'>
+                    <label htmlFor="title" className='publish-form__title-label'>TITLE YOUR VIDEO</label>
+                    <input id='title-input' type="text" name="title" placeholder="Add a title to your video"></input>
+                    <label htmlFor="description" className='publish-form__description-label'>ADD A VIDEO DESCRIPTION</label>
+                    <textarea 
+                        className='description-input' 
+                        type="text" 
+                        name="description" 
+                        placeholder="Add a description to your video"
+                        id={videoId}>
+                    </textarea>
+                </div>
             </div>
-            <PublishButton/>
-        </section>
+            <div className='button-container'>
+            {/* <Link to="/" className='publish-button-container'> */}
+                <button type="submit" className='publish-button'>
+                    <img className="publish-button__icon" src={publishIcon} alt="Button Icon"/>
+                    PUBLISH
+                </button>
+            {/* </Link> */}
+            <div className='cancel-button-container'>
+                <button type="button" className='cancel-button'>
+                    CANCEL
+                </button>
+            </div>
+        </div>
+        </form>
     </>
     )
 }
 
-export default VideoUpload;
+export default VideoUpload; 
